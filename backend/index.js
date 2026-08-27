e7edeca19844fff5ccc6fc2a28dd56a6d0ecb83a
kostya noskov <kostay_noskov_97@mail.ru>
2026-08-27 13:40:16 +0500

    ka

--- backend/index.js
+++ backend/index.js
@@ -14,23 +14,7 @@ export const supabase = createClient(
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-    next();
-});
-
-app.use(cors({
-    origin: ['https://weekwork-app.vercel.app', 'https://t.me', 'https://web.telegram.org'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(employerRoutes);
