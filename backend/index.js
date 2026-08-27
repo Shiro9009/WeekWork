27d27a306359707f1314b36f6dd2f5485e83889f
kostya noskov <kostay_noskov_97@mail.ru>
2026-08-27 13:51:55 +0500

    sol

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,17 @@ export const supabase = createClient(
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
