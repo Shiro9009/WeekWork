a15b4efeb09d40175412d8078cb9378ad7968c21
kostya noskov <kostay_noskov_97@mail.ru>
2026-09-21 16:42:04 +0500

    newTookenBot

--- backend/bot.js
+++ backend/bot.js
@@ -1,5 +1,5 @@
 import TelegramBot from 'node-telegram-bot-api';
 
-export const BOT_TOKEN = '8854559282:AAFwPAD4gQLNMDFihLBF6lATZmsbIiiYQuA';
+export const BOT_TOKEN = '8854559282:AAFU_3etP-Y_m5IERAulk4RnKYh7DuOfbT8';
 export const bot = new TelegramBot(BOT_TOKEN, { polling: true });
 export const APP_URL = 'https://weekwork-app.vercel.app';
